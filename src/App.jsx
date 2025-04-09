0b29a4bd7000a522b55b20ad66b6d160f9379050
Angela Yu <angelayu0530@gmail.com>
2025-04-09 19:41:37 -0400

    trying to set up pages again

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import '../public/Minecraft/dist/minecraft-css-framework-v-1.0.0.css'
+import React from 'react';
 import Header from './components/Header.jsx';
 import About from './components/About.jsx';
+import '../public/Minecraft/dist/minecraft-css-framework-v-1.0.0.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Header /> {}
-      <Routes>
-        <Route path="/" element={<About />} />
-        {}
-      </Routes>
-    </Router>
+    <Router basename="/myprofilo--">
+    <Header />
+    <Routes>
+      <Route path="/" element={<About />} />
+    </Routes>
+  </Router>
   );
 }
 
