5fe3ed5425c22a27a661cfb40cfbab2da3d0473d
Angela Yu <angelayu0530@gmail.com>
2025-04-09 20:13:54 -0400

    "trying to set router"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header.jsx';
 import About from './components/About.jsx';
-import '../public/Minecraft/dist/minecraft-css-framework-v-1.0.0.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
